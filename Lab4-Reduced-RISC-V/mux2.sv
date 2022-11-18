d00b5351b065e6e42ba680a402a3699c81aa473a
jym521 <junyumeng@outlook.com>
2022-11-18 11:45:14 +0000

    Initial commit

---
+++ Lab4-Reduced-RISC-V/mux2.sv
@@ -0,0 +1,9 @@
+module mux2 (
+    input logic [31:0]        RegOp2, ImmOp,//RD2 from Reg file; immediate value from instruction
+    input logic              ALUsrc, //selector
+    output logic [31:0]       ALUop2 //mux output  
+);
+
+assign ALUop2 = ALUsrc ? ImmOp : RegOp2;
+
+endmodule 
