69bd809945feb806134dc0e53d86e6e9e320d187
jym521 <junyumeng@outlook.com>
2022-12-02 14:55:46 +0000

    changed back to reduced RISC-V

--- Lab4-Reduced-RISC-V/alu.sv
+++ Lab4-Reduced-RISC-V/alu.sv
@@ -1,32 +1,12 @@
-module alu #(
-    parameter WIDTH = 32
-)(
-    input logic     [2:0]            ALUctrl,
-    input logic     [WIDTH-1:0]      ALUop1, ALUop2,
-    output logic    [WIDTH-1:0]      ALUout,
-    output logic                     EQ
+module alu (
+    input logic                 ALUctrl,
+    input logic     [31:0]      ALUop1, ALUop2,
+    output logic    [31:0]      ALUout,
+    output logic                EQ
 
 );
 
-always_comb begin
-    if (ALUctrl == 3'b000) begin //add
-        ALUout = ALUop1 + ALUop2;
-    end
-    else if (ALUctrl == 3'b001) begin //subtract
-        ALUout = ALUop1 - ALUop2;
-    end
-    else if (ALUctrl == 3'b010) begin //and
-        ALUout = ALUop1 && ALUop2;
-    end
-    else if (ALUctrl == 3'b011) begin //or
-        ALUout = ALUop1 || ALUop2;
-    end
-    else if (ALUctrl == 3'b101) begin //SLT
-        ALUout = ALUop1 < ALUop2;
-    end
-end
-
-
+assign ALUout = ALUop1 + ALUop2;
 assign EQ = (ALUop1 == ALUop2);
 
 endmodule
--- Lab4-Reduced-RISC-V/mux2.sv
+++ Lab4-Reduced-RISC-V/mux2.sv
@@ -1,9 +1,9 @@
 module mux2 (
-    input logic [31:0]        RegOp2, ImmExt,//RD2 from Reg file; immediate value from instruction after extension
-    input logic               ALUsrc, //selector
-    output logic [31:0]       ALUResult //mux output  
+    input logic [31:0]        RegOp2, ImmOp,//RD2 from Reg file; immediate value from instruction
+    input logic              ALUsrc, //selector
+    output logic [31:0]       ALUop2 //mux output  
 );
 
-assign ALUResult = ALUsrc ? ImmExt : RegOp2;
+assign ALUop2 = ALUsrc ? ImmOp : RegOp2;
 
 endmodule 
